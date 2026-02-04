4618c11271c0d27fc77803aaae755592d104d15c
siddhi-gargs <gargsiddhi2005@gmail.com>
2026-02-04 22:57:11 +0530

    Render all the books

--- src/BooksProvider.js
+++ src/BooksProvider.js
@@ -3,12 +3,12 @@ import React, { useState, createContext, useEffect } from "react";
 const BookContext = createContext([]);
 
 export default function BookProvider({ children }) {
-  const [bookData, setBookData] = useState();
+  const [bookData, setBookData] = useState([]);
   useEffect(() => {
-    fetch("./books.json")
+    fetch("/books.json")
       .then((response) => response.json())
       .then((data) => setBookData(data));
-  });
+  }, []);
 
   return (
     <BookContext.Provider value={bookData}>{children}</BookContext.Provider>
